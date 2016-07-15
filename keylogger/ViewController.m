b08cdf443fc1ba304fe54c895c765719fa13cc39
Michael Colon <michaelcolon@youaredreaminginspacewheretimeisjustenergy.local>
2016-07-15 12:31:06 -0400

    Initial Commit

---
+++ keylogger/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  keylogger
+//
+//  Created by Michael Colon on 7/15/16.
+//  Copyright © 2016 Michael Colon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
---
+++ keylogger/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  keylogger
+//
+//  Created by Michael Colon on 7/15/16.
+//  Copyright © 2016 Michael Colon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
